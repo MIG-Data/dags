73ee90666c2fbfb556de5d0f0ebc6f5e1db5fd87
MIG-Data <61855671+MIG-Data@users.noreply.github.com>
2020-03-26 22:45:42 -0700

    Add files via upload

---
+++ WGO_dag.py
@@ -0,0 +1,41 @@
+
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 26 14:02:41 2020
+
+@author: tcai
+"""
+
+"""
+S3 Sensor Connection Test
+"""
+import airflow
+from airflow import DAG
+from airflow.operators.http_operator import SimpleHttpOperator
+from airflow.sensors.http_sensor import HttpSensor
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.email_operator import EmailOperator
+from airflow.contrib.operators.databricks_operator import DatabricksSubmitRunOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 3, 25),
+    'end_date': None,
+    'email': ['tcai@migcap.com', 'yjeon@migcap.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG('WGO_dag', default_args=default_args, schedule_interval= '@weekly')
+
+secform4_sh = BashOperator(
+    task_id='WGO_RV_Trader_SCRAPE',
+    bash_command="python3 /home/ec2-user/WGO/rv_data_scrape.py ",
+    queue="pipeline2",
+    dag=dag)
+
