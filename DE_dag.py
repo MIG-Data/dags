e19221c817353ff2b129ef10b7f351c0b1945443
MIG-Data <61855671+MIG-Data@users.noreply.github.com>
2020-04-20 19:32:39 -0700

    Create DE_dag.py

---
+++ DE_dag.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 24 14:02:41 2020
+@author: yjeon
+"""
+
+"""
+S3 Sensor Connection Test
+"""
+import airflow
+from airflow import DAG
+from airflow.operators.http_operator import SimpleHttpOperator
+from airflow.sensors.http_sensor import HttpSensor
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.email_operator import EmailOperator
+from airflow.contrib.operators.databricks_operator import DatabricksSubmitRunOperator
+
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'MIG',
+    'depends_on_past': False,
+    'start_date': datetime(2020,4,20),
+    'end_date': None,
+    'email': ['tcai@migcap.com', 'yjeon@migcap.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG('DE', default_args=default_args, schedule_interval= '0 0 * * MON')
+
+de_sh = BashOperator(
+    task_id='SCRAPE',
+    bash_command="source /home/ec2-user/.venv/bin/activate && python /home/ec2-user/DE/inventory.py ",
+    queue="pipeline4",
+    dag=dag)
