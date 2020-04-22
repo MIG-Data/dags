695149bd9d5db98776da6bf2dd07076d08306c16
MIG-Data <61855671+MIG-Data@users.noreply.github.com>
2020-04-21 23:57:47 -0700

    Add files via upload

--- DE_dag.py
+++ DE_dag.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb 24 14:02:41 2020
-@author: yjeon
+Created on Tue Apr 21 23:54:14 2020
+
+@author: tcai
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 26 14:02:41 2020
+
+@author: tcai
 """
 
 """
@@ -14,13 +22,12 @@ from airflow.sensors.http_sensor import HttpSensor
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.email_operator import EmailOperator
 from airflow.contrib.operators.databricks_operator import DatabricksSubmitRunOperator
-
 from datetime import datetime, timedelta
 
 default_args = {
-    'owner': 'MIG',
+    'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2020,4,20),
+    'start_date': datetime(2020, 4, 20),
     'end_date': None,
     'email': ['tcai@migcap.com', 'yjeon@migcap.com'],
     'email_on_failure': True,
@@ -29,10 +36,26 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-dag = DAG('DE', default_args=default_args, schedule_interval= '0 0 * * MON')
+dag = DAG('DE_dag', default_args=default_args, schedule_interval= '@daily')
 
-de_sh = BashOperator(
-    task_id='SCRAPE',
-    bash_command="source /home/ec2-user/.venv/bin/activate && python /home/ec2-user/DE/inventory.py ",
-    queue="pipeline4",
+DE_sh = BashOperator(
+    task_id='DE_Equipment_Trader_SCRAPE',
+    bash_command="python3 /home/ec2-user/DE/equipment_trader.py ",
+    queue="pipeline2",
     dag=dag)
+
+'''
+notebook_task_params = {
+        'existing_cluster_id': '0128-230140-huts317', # cluster id of MIG Cluster 2
+        'notebook_task': {
+                'notebook_path': '/Users/garquette@migcap.com/WGO_inventory_analysis'
+                }
+        }
+WGO_notebook_task = DatabricksSubmitRunOperator(
+        task_id = 'WGO_notebook_task',
+        dag = dag,
+        queue = 'pipeline2',
+        json = notebook_task_params
+        )
+WGO_sh.set_downstream(WGO_notebook_task)
+'''
