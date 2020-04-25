fbf8096ad705900685b2486dead9c81de406952e
MIG-Data <61855671+MIG-Data@users.noreply.github.com>
2020-04-24 20:41:52 -0700

    Update HCSG_dag.py

--- HCSG_dag.py
+++ HCSG_dag.py
@@ -19,7 +19,7 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-dag = DAG('CA_DPH_COVID', default_args=default_args, schedule_interval= '@daily')
+dag = DAG('HCSG', default_args=default_args, schedule_interval= '@daily')
 
 five_sh = BashOperator(
     task_id='SCRAPE',
