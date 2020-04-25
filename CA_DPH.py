838052c4cd4155c6ae417e631141cdd85627b63b
MIG-Data <61855671+MIG-Data@users.noreply.github.com>
2020-04-24 20:31:37 -0700

    Create CA_DPH.py

---
+++ CA_DPH.py
@@ -0,0 +1,28 @@
+import airflow
+from airflow import DAG
+from airflow.operators.http_operator import SimpleHttpOperator
+from airflow.sensors.http_sensor import HttpSensor
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.email_operator import EmailOperator
+from airflow.contrib.operators.databricks_operator import DatabricksSubmitRunOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 4, 24),
+    'end_date': None,
+    'email': ['tcai@migcap.com', 'yjeon@migcap.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG('CA_DPH_COVID', default_args=default_args, schedule_interval= '* */6 * * *')
+
+five_sh = BashOperator(
+    task_id='SCRAPE',
+    bash_command="python3 /home/ec2-user/Scrapes/HCSG/PYTHON/CA_DPH.py ",
+    queue="pipeline1",
+    dag=dag)
