48c9aea994410ccdd7aa6696e5ff4fa598218ac8
MIG-Data <61855671+MIG-Data@users.noreply.github.com>
2020-07-17 12:19:45 -0700

    Add files via upload

---
+++ fetch_store_detail_dag.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 24 14:02:41 2020
+
+@author: tcai
+"""
+
+"""
+S3 Sensor Connection Test
+"""
+import airflow
+from airflow import DAG
+from airflow.operators.http_operator import SimpleHttpOperator
+from airflow.sensors.http_sensor import HttpSensor
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.email_operator import EmailOperator
+from airflow.contrib.operators.databricks_operator import DatabricksSubmitRunOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 7, 17),
+    'end_date': None,
+    'email': ['tcai@migcap.com', 'yjeon@migcap.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG('fetch_store_detail_dag', default_args=default_args, schedule_interval= '@weekly')
+
+fetch_store_detail_sh = BashOperator(
+    task_id='FETCH_STORE_DETAIL_SCRAPE',
+    bash_command="python3 /home/ec2-user/store_turnover/fetch_store_detail.py ",
+    queue="pipeline1",
+    dag=dag)
+
+
